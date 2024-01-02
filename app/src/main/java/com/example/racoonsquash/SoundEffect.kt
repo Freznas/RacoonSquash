43e0f4abf3f82ca15fae606551ceb3646e3469ad
jhaard <jorgen.hard@gmail.com>
2024-01-02 15:40:37 +0100

    Renamed wav-files and changed when to play them.

--- app/src/main/java/com/example/racoonsquash/SoundEffect.kt
+++ app/src/main/java/com/example/racoonsquash/SoundEffect.kt
@@ -8,8 +8,8 @@ class SoundEffect (context: Context) : Sound {
     // Soundpool-objekt med max antal ljud som kan spelas samtidigt
     private val soundPool: SoundPool = SoundPool.Builder().setMaxStreams(10).build()
 
-    private val bounceSound: Int = soundPool.load(context, R.raw.paddlebounce, 0)
-    private val breakSound: Int = soundPool.load(context, R.raw.wallbounce, 1)
+    private val bouncePaddleSound: Int = soundPool.load(context, R.raw.paddlebounce, 0)
+    private val wallBounceSound: Int = soundPool.load(context, R.raw.wallbounce, 1)
     private val gameOverSound: Int = soundPool.load(context, R.raw.lose, 2)
     private val upSound: Int = soundPool.load(context, R.raw.up, 1)
 
@@ -21,8 +21,8 @@ class SoundEffect (context: Context) : Sound {
         this.audioID = id
 
         when(id) {
-            0 -> soundPool.play(bounceSound, 1.0f, 1.0f, 0, 0, 1.0f)
-            1 -> soundPool.play(breakSound, 1.0f, 1.0f, 1, 0, 1.0f)
+            0 -> soundPool.play(bouncePaddleSound, 1.0f, 1.0f, 0, 0, 1.0f)
+            1 -> soundPool.play(wallBounceSound, 1.0f, 1.0f, 1, 0, 1.0f)
             2 -> soundPool.play(gameOverSound, 1.0f, 1.0f, 1, 0, 1.0f)
             3 -> soundPool.play(upSound, 1.0f, 1.0f, 1, 0, 1.0f)
         }
