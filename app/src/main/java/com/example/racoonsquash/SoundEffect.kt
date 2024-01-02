bab93927fde2144a589a82bfa9f49111b165206f
jhaard <jorgen.hard@gmail.com>
2024-01-02 15:43:37 +0100

    Changed priority settings.

--- app/src/main/java/com/example/racoonsquash/SoundEffect.kt
+++ app/src/main/java/com/example/racoonsquash/SoundEffect.kt
@@ -8,8 +8,8 @@ class SoundEffect (context: Context) : Sound {
     // Soundpool-objekt med max antal ljud som kan spelas samtidigt
     private val soundPool: SoundPool = SoundPool.Builder().setMaxStreams(10).build()
 
-    private val bouncePaddleSound: Int = soundPool.load(context, R.raw.paddlebounce, 0)
-    private val wallBounceSound: Int = soundPool.load(context, R.raw.wallbounce, 1)
+    private val bouncePaddleSound: Int = soundPool.load(context, R.raw.paddlebounce, 1)
+    private val wallBounceSound: Int = soundPool.load(context, R.raw.wallbounce, 0)
     private val gameOverSound: Int = soundPool.load(context, R.raw.lose, 2)
     private val upSound: Int = soundPool.load(context, R.raw.up, 1)
 
@@ -21,9 +21,9 @@ class SoundEffect (context: Context) : Sound {
         this.audioID = id
 
         when(id) {
-            0 -> soundPool.play(bouncePaddleSound, 1.0f, 1.0f, 0, 0, 1.0f)
-            1 -> soundPool.play(wallBounceSound, 1.0f, 1.0f, 1, 0, 1.0f)
-            2 -> soundPool.play(gameOverSound, 1.0f, 1.0f, 1, 0, 1.0f)
+            0 -> soundPool.play(bouncePaddleSound, 1.0f, 1.0f, 1, 0, 1.0f)
+            1 -> soundPool.play(wallBounceSound, 1.0f, 1.0f, 0, 0, 1.0f)
+            2 -> soundPool.play(gameOverSound, 1.0f, 1.0f, 2, 0, 1.0f)
             3 -> soundPool.play(upSound, 1.0f, 1.0f, 1, 0, 1.0f)
         }
     }
