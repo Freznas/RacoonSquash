52d7adc832831a6225d3fa20e5df0520758c98be
jhaard <jorgen.hard@gmail.com>
2024-01-03 12:10:56 +0100

    Seems Squashpad inherits from BallSquash, must cleanup this too.

--- app/src/main/java/com/example/racoonsquash/BallSquash.kt
+++ app/src/main/java/com/example/racoonsquash/BallSquash.kt
@@ -5,7 +5,7 @@ import android.graphics.Canvas
 import android.graphics.Paint
 import android.graphics.Rect
 
-class BallSquash(
+open class BallSquash(
     context: Context,
     posX: Float,
     posY: Float,
